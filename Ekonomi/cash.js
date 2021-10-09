ec61fd6061e3807f75e5d10f9bf31b1c6f00e397
Mhamadd11227H <89313003+Mhamadd11227H@users.noreply.github.com>
2021-10-10 00:26:20 +0300

    Update cash.js

--- Ekonomi/cash.js
+++ Ekonomi/cash.js
@@ -13,7 +13,7 @@ message.channel.send(new Discord.MessageEmbed()
                   .setFooter('Coin!')
                   .setThumbnail('https://media.discordapp.net/attachments/852987040659931248/871313810441510922/image0.gif')
                   .setAuthor(user.tag, user.avatarURL({dynamic: true}))     
-                  .addField(`Currently have 💰 `,` 🪙 __${toplam ? toplam +'':`0`}__`,true))
+                  .addField(`Currently have 💰 `,` 💵 __${toplam ? toplam +'':`0`}__`,true))
                     
   }
 
