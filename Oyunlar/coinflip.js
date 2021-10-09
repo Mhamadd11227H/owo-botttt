943dbd64ca8a81c5335f3e73a5abe266ff12f3a6
Mhamadd11227H <89313003+Mhamadd11227H@users.noreply.github.com>
2021-10-09 23:46:33 +0300

    Update coinflip.js

--- Oyunlar/coinflip.js
+++ Oyunlar/coinflip.js
@@ -46,7 +46,7 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
      if (awnser === "LOOSELOOSE") {
  var kaybettin = miktar*2        
         //////        
-        var text2 = `**<{name}${message.author.id}> CoinFlip 🤔 Choice win Or Lost**`;
+        var text2 = `**<@${message.author.id}> CoinFlip 🤔 Choice win Or Lost**`;
           message.channel.send(text2).then(msg => {
             setTimeout(() => {
               msg.edit(`**<@${message.author.id}>spent cowoncy 🪙 ${-kaybettin} and chose heads
