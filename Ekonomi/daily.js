b0c105205f31a2d4f64b85fb83e54acdf2e21f10
Mhamadd11227H <89313003+Mhamadd11227H@users.noreply.github.com>
2021-10-10 00:27:25 +0300

    Update daily.js

--- Ekonomi/daily.js
+++ Ekonomi/daily.js
@@ -30,7 +30,7 @@ let moneys = rastgeleMiktar(5000, 8000);
                    .setFooter('Kraken Daily')
                    .setThumbnail('https://media.discordapp.net/attachments/859402172373925918/870071235248263168/image0.gif')
                    .setAuthor(`Daily Crate Claimed`)
-                   .setDescription(`<✅> Here is your daily 🪙 __${moneys}__ was added to your balance`))
+                   .setDescription(`<✅> Here is your daily 💵 __${moneys}__ was added to your balance`))
 
 db.set(`worktime_${message.author.id}`, Date.now())
 
