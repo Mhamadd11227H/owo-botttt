cf6e0389ee2dd55ed7a2e53bece74ef04a9efa85
Mhamadd11227H <89313003+Mhamadd11227H@users.noreply.github.com>
2021-10-10 00:21:36 +0300

    Update beercode.js

--- beercode.js
+++ beercode.js
@@ -82,7 +82,7 @@ client.on("message", message => {
   if (message.content === ">help") {
     const embed = new Discord.MessageEmbed()
       .setThumbnail(client.user.avatarURL())
-      .setColor("PINK")
+      .setColor("e90000")
       .setAuthor("The Prefix { > }").setDescription(`
 **<a:👤:┇User Commands**
 > bot - avatar - server
